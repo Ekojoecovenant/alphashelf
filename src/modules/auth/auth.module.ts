bb494340148ef7ee84522fa5caec97192fcacd3e
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-15 21:41:33 +0000

    fix: Added UsersModule to auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
