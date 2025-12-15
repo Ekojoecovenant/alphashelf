bfc8ae0dcbbf2b5bf864772877eec069b9ed19db
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-15 18:43:14 +0000

    feat: add jwt strategy and auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtModule],
+})
 export class AuthModule {}
