a83c1ba3ab0f97ac94488f9ba2277c677fe781b4
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-15 13:31:08 +0000

    chore: scaffold core domain modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { BooksModule } from './modules/books/books.module';
+import { AuthorsModule } from './modules/authors/authors.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BooksModule, AuthorsModule, CategoriesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
